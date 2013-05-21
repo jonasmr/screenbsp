77de38685215ef80d2ceaca3b26f3347885389c8
jonasm <jonasm@localhost>
2013-05-21 23:22:50 +0200

    mesh drawing

--- mesh.h
+++ mesh.h
@@ -14,6 +14,16 @@ enum EBaseMesh
 	MESH_SPHERE_6,
 	MESH_SPHERE_7,
 	MESH_SPHERE_8,
+	MESH_BOX_FLAT,
+	MESH_SPHERE_1_FLAT,
+	MESH_SPHERE_2_FLAT,
+	MESH_SPHERE_3_FLAT,
+	MESH_SPHERE_4_FLAT,
+	MESH_SPHERE_5_FLAT,
+	MESH_SPHERE_6_FLAT,
+	MESH_SPHERE_7_FLAT,
+	MESH_SPHERE_8_FLAT,
+
 	MESH_SIZE,
 };
 
@@ -32,6 +42,9 @@ struct Mesh
 	uint32 	nIndices;
 	const Vertex* 	pVertices;
 	const uint16* 	pIndices;
+
+	uint32 IndexBuffer;
+	uint32 VertexBuffer;
 };
 
 void MeshInit();
